94664ac6d572719be9450429b88cb2279c5acccd
hnz678 <zhenghualin2008@hotmail.com>
2021-03-10 23:54:56 -0500

    adding Beer domain and repository. Close #6

---
+++ src/main/java/com/example/springrest/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.example.springrest.msscbeerservice.repositories;
+
+import com.example.springrest.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
